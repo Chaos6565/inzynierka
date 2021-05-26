28c5b41b1c7dc01e4ba4c8d3cd25c666c23e067e
Chaos6565 <mateuszszcz65@gmail.com>
2021-05-26 18:58:14 +0200

    Fixed Colliders/Move

--- Projekt/Assets/Scripts/PlayerController.cs
+++ Projekt/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Animator animation;
-    Rigidbody2D rb2d;
+    Animator animation2d;
+    Rigidbody2D rigidbody2d;
     SpriteRenderer spriteRen;
 
     float runSpeed = 7;
@@ -14,25 +14,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animation = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
+        animation2d = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         spriteRen = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        Vector2 move_velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rigidbody2d.velocity = move_velocity * runSpeed;
 
-        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed, Input.GetAxisRaw("Vertical") * runSpeed);
-
-        animation.SetFloat("MoveX", rb2d.velocity.x);
-        animation.SetFloat("MoveY", rb2d.velocity.y);
+        animation2d.SetFloat("MoveX", rigidbody2d.velocity.x);
+        animation2d.SetFloat("MoveY", rigidbody2d.velocity.y);
 
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1
             || Input.GetAxisRaw("Vertical") == -1)
         {
-            animation.SetFloat("lastX", Input.GetAxisRaw("Horizontal"));
-            animation.SetFloat("lastY", Input.GetAxisRaw("Vertical"));
+            animation2d.SetFloat("lastX", Input.GetAxisRaw("Horizontal"));
+            animation2d.SetFloat("lastY", Input.GetAxisRaw("Vertical"));
         }
 
     }

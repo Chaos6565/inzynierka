4d7e32df8d5148061ec8dd3eeae1f15035d752c8
robpietryk <robpietryk@gmail.com>
2021-06-07 15:15:40 +0200

    Quick Form Fix

--- WMI/Assets/Scripts/InteractableObjects/FormBottomHandler.cs
+++ WMI/Assets/Scripts/InteractableObjects/FormBottomHandler.cs
@@ -22,7 +22,6 @@ public class FormBottomHandler : MonoBehaviourPun
     [Header("Zmienne do okna rezultatu")]
     [SerializeField] private Animator _animation = null;
     [SerializeField] private GameObject door = null;
-    [SerializeField] private GameObject trigger = null;
 
     private bool check;
     private string nameInput;
@@ -49,22 +48,15 @@ public class FormBottomHandler : MonoBehaviourPun
         
     }
 
-
-
     public void ClickEndingButton()
     {
 
         if (check == true && nameInput != "" && option == "Opcja trzecia - ta do wygrania")
         {
 
-            //photonView.RPC("AddToSuccessCounter", RpcTarget.All);
-            //if (photonView.IsMine)
-            //{
-                CompleteForm();
-            //}
+            CompleteForm();
             background.SetActive(true);
             resultWindowPass.SetActive(true);
-            
 
         }
         else
@@ -112,25 +104,23 @@ public class FormBottomHandler : MonoBehaviourPun
         if (obj.Code == COMPLETE_FORM_EVENT)
         {
             AddToSuccessCounter();
-            Debug.LogError("Received Event");
+            Debug.Log("Received Event");
         }
     }
 
     private void CompleteForm()
     {
-        Debug.LogError("Form Completed");
+        Debug.Log("Form Completed");
         Photon.Realtime.RaiseEventOptions raiseEventOptions = new Photon.Realtime.RaiseEventOptions { Receivers = Photon.Realtime.ReceiverGroup.Others };
         ExitGames.Client.Photon.SendOptions sendOptions = new ExitGames.Client.Photon.SendOptions { };
         PhotonNetwork.RaiseEvent(COMPLETE_FORM_EVENT, new object[] { }, raiseEventOptions, sendOptions);
         AddToSuccessCounter();
-        //photonView.RPC("AddToSuccessCounter", RpcTarget.All);
     }
 
-    //[PunRPC]
     public void AddToSuccessCounter()
     {
         successCounter += 1;
-        Debug.LogError($"Add Success! Current value: {successCounter}");
+        Debug.Log($"Add Success! Current value: {successCounter}");
     }
 
     [PunRPC]
@@ -138,7 +128,6 @@ public class FormBottomHandler : MonoBehaviourPun
     {
         _animation.SetBool("isOpen", true);
         door.GetComponent<BoxCollider2D>().enabled = false;
-        trigger.SetActive(false);
     }
 
 

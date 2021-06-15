50ff2dc795f5e46081feccc875a4334c711bc0c5
robpietryk <robpietryk@gmail.com>
2021-06-15 23:08:33 +0200

    Some fixes to player spawn

--- WMI/Assets/Scripts/Player/PlayerSpawner.cs
+++ WMI/Assets/Scripts/Player/PlayerSpawner.cs
@@ -1,33 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
 using UnityEngine;
 
 namespace WMI
 {
-    public class PlayerSpawner : MonoBehaviour
+    public class PlayerSpawner : MonoBehaviourPun
     {
         [SerializeField] private GameObject[] playerPrefab = null;
         [SerializeField] public GameObject[] spawnPoints = null;
 
         CameraScript cameraScript;
 
-        int numberPlayers = 0;
-
         // Start is called before the first frame update
         private void Start()
         {
+            
             cameraScript = FindObjectOfType<CameraScript>();
 
-            GameObject player = PhotonNetwork.Instantiate(playerPrefab[DeterminePlayerSpawnPoint()].name, spawnPoints[DeterminePlayerSpawnPoint()].transform.position, Quaternion.identity);
-            cameraScript.SetCameraTarget(player.transform);
-        }
+            //Debug.LogError($"Player number: {PhotonNetwork.LocalPlayer.ActorNumber}");
 
-        private int DeterminePlayerSpawnPoint()
-        {
-            numberPlayers = PhotonNetwork.CountOfPlayers;
-
-            return numberPlayers - 1;
+            if (PhotonNetwork.LocalPlayer.IsLocal)
+            {
+                GameObject player = PhotonNetwork.Instantiate(playerPrefab[PhotonNetwork.LocalPlayer.ActorNumber - 1].name, spawnPoints[PhotonNetwork.LocalPlayer.ActorNumber - 1].transform.position, Quaternion.identity);
+            
+                cameraScript.SetCameraTarget(player.transform);
+            }
         }
     }
 }

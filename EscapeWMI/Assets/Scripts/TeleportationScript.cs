508b48d039d63fd1eb80abb923015367060d6fe6
laurapiot <82157836+laurapiot@users.noreply.github.com>
2021-10-11 00:38:10 +0200

    Teleportation Script

---
+++ EscapeWMI/Assets/Scripts/TeleportationScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationScript : MonoBehaviour
+{
+    //public GameObject teleportationObject;
+    public Transform destiny;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.position = destiny.transform.position;
+    }
+}

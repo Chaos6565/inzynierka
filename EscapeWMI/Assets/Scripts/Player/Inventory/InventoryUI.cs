ca4e14b53a78c30fd7013c3f4e153817823dc9aa
laurapiot <82157836+laurapiot@users.noreply.github.com>
2021-12-21 06:26:51 +0100

    współdzielenie przedmiotów - work in progress

--- EscapeWMI/Assets/Scripts/Player/Inventory/InventoryUI.cs
+++ EscapeWMI/Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -1,22 +1,29 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using Photon.Pun;
 
-public class InventoryUI : MonoBehaviour
+public class InventoryUI : MonoBehaviourPun
 {
 
     private Inventory inventory;
     private Transform itemSlotContainer;
     private Transform itemSlotTemplate;
     private Transform itemDisplay;
+    public Transform sharedItemDisplayNote;
+    public Transform sharedItemDisplayCloseButton;
     public PlayerController player;
 
     public void Init()
     {
         itemSlotContainer = transform.Find("ItemSlotContainer");
         itemSlotTemplate = itemSlotContainer.Find("ItemSlotTemplate");
+        sharedItemDisplayNote = GameObject.Find("Shared Item Display Canvas").transform.Find("SharedItemDisplay").Find("Note");
+        sharedItemDisplayCloseButton = GameObject.Find("Shared Item Display Canvas").transform.Find("SharedItemDisplay").Find("CloseButton");
 
         itemSlotTemplate.gameObject.SetActive(false);
+        sharedItemDisplayNote.gameObject.SetActive(false);
+        sharedItemDisplayCloseButton.gameObject.SetActive(false);
 
     }
 
@@ -60,17 +67,16 @@ public class InventoryUI : MonoBehaviour
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
 
-
-
             itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, -y * itemSlotCellSize);
             Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
             image.sprite = item.GetSprite();
 
             itemDisplay = itemSlotRectTransform.Find("ItemDisplay");
+            itemDisplay.gameObject.SetActive(true);
             itemDisplay.gameObject.SetActive(false);
             Image note = itemDisplay.Find("Note").GetComponent<Image>();
             note.sprite = item.GetDisplay();
-            
+
             x++;
             if (x >= 4)
             {
@@ -80,6 +86,23 @@ public class InventoryUI : MonoBehaviour
         }
 
         itemSlotTemplate.gameObject.SetActive(false);
+
+    }
+
+
+    public void ShareNote(Image note)
+    {
+        photonView.RPC("RpcShareNote", RpcTarget.Others, note.sprite);
+    }
+
+    [PunRPC]
+    public void RpcShareNote(Sprite sharedNote)
+    {
+        
+        
+        GameObject.Find("Shared Item Display Canvas").transform.Find("SharedItemDisplay").GetChild(0).GetComponent<Image>().sprite = sharedNote;    //GetChild(0) = sharedItemDisplayNote
+        GameObject.Find("Shared Item Display Canvas").transform.Find("SharedItemDisplay").GetChild(0).gameObject.SetActive(true);
+        GameObject.Find("Shared Item Display Canvas").transform.Find("SharedItemDisplay").GetChild(1).gameObject.SetActive(true);                   //GetChild(1) = sharedItemDisplayCloseButton
 
     }
 
